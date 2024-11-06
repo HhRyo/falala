4ea03be9ce10e86fe5531e6b1381bf8d544ed1b2
HhRyo <88109093+HhRyo@users.noreply.github.com>
2024-11-05 21:24:18 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from 'react';
 import Users from './components/Users';
-import Signup from './components/Signup'; // Adjust path as needed
+import Signup from './components/Signup';
 
 type User = {
   id: number;
@@ -19,20 +19,24 @@ const USERS_INIT: User[] = [
     username: 'blightyear',
     email: 'buzz.lightyear@gmail.com',
     password: 'password',
-    imageUrl: 'https://scontent-atl3-1.xx.fbcdn.net/v/t39.30808-6/354028274_877327910424573_1574551018727772204_n.png?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=WrKGYikcYXoQ7kNvgGeB4_x&_nc_zt=23&_nc_ht=scontent-atl3-1.xx&_nc_gid=AHKbySkyVuOwGM7c8dVPdUM&oh=00_AYA9v6x3g02rlI75wfbcbePieDyMpZyXGOGMSAV9I_rfIQ&oe=67249BCA',
+    imageUrl: 'https://example.com/image.png',
   },
 ];
 
 export default function Home() {
+  // Manages the state of users - (Requirement: 25 pts for triggering rendering)
+  const [users, setUsers] = useState<User[]>(USERS_INIT);
+
+  // Handler for adding a user - (Requirement: Passing input data from Signup to Home - 20 pts)
+  const addUser = (user: Omit<User, 'id'>) => {
+    setUsers([...users, { ...user, id: users.length + 1 }]);
+  };
 
   return (
-    <div>
-    
-      <div className="flex flex-col items-center justify-center p-2 bg-gray-100">
-        <h1 className="text-4xl font-bold mb-6">Welcome to the Home Page</h1>
-        <Users />
-      </div>
-      <Signup />
+    <div className="flex flex-col items-center justify-center p-2 bg-gray-100">
+      <h1 className="text-4xl font-bold mb-6">Welcome to the Home Page</h1>
+      <Users users={users} />
+      <Signup onAddUser={addUser} />
     </div>
   );
 }
